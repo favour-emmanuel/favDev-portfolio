683f3c46c38cb1d5f85f97c9fec6eafdd30d0910
Favour Emmanuel <iyenefavour@gmail.com>
2024-09-02 12:18:52 +0100

    removed unused import

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import SkillsPage from "../pages/SkillsPage";
 
 const Layout = () => {
   return (
